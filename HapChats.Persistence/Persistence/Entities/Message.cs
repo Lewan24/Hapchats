108c0faf167ef9e7e9705d09599e5dd4f7a10f96
Artur Lewandowski <LewanMordor@protonmail.com>
2023-01-26 09:54:32 +0100

    Architecture changes, more content
    Change project name from persistence to Domain
    Add content like classes for database
    Add DbContext
    Add default localhost connection

---
+++ HapChats.Persistence/Persistence/Entities/Message.cs
@@ -0,0 +1,17 @@
+﻿using HapChats.Domain.Persistence.Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace HapChats.Domain.Persistence.Entities;
+
+public record Message : Auditable
+{
+    [Required]
+    [StringLength(255)]
+    public string Msg { get; set; }
+
+    [Required]
+    public Guid UserId { get; set; }
+
+    [Required]
+    public Guid Chat { get; set; }
+}
---
+++ Infractucture/DbContexts/ChatDbContext.cs
@@ -0,0 +1,37 @@
+﻿using HapChats.Domain.Persistence.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace HapChats.Infractucture.DbContexts;
+
+public class ChatDbContext : DbContext
+{
+    private readonly IConfiguration _config;
+
+    public ChatDbContext(DbContextOptions<ChatDbContext> options, IConfiguration config) : base(options)
+    {
+        _config = config;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+
+        var conn = _config.GetConnectionString("DefaultConnection");
+
+        optionsBuilder.UseNpgsql(conn);
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserProfile> UsersProfiles { get; set; }
+    public DbSet<UserFriend> UserFriends { get; set; }
+    public DbSet<FriendRequest> FriendsRequests { get; set; }
+    public DbSet<ChatBox> ChatBoxes { get; set; }
+    public DbSet<GroupChatBox> GroupChatBoxes { get; set; }
+    public DbSet<Message> Messages { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
