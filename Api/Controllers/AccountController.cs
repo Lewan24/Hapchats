e9212210bc9200958bca40f38809d6d2a74685f7
Lewan24 <LewanMordor@protonmail.com>
2023-01-29 13:45:16 +0100

    Add Account controller with Delete account method. Implement MediatR

---
+++ Api/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MediatR;
+using Api.Functions.Account.Commands;
+
+namespace Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public AccountController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAccount(Guid id)
+    {
+        var result = await _mediator.Send(new DeleteAccountQuery());
+
+        return result is true ? Ok("Successfully deleted account,") : Ok("Unfortunately cant delete account. Try again later.");
+    }
+}
